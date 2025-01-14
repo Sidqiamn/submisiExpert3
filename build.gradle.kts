1881d591345fc50885cac21ca5c7630add1ec96c
qiamn <89834713+Sidqiamn@users.noreply.github.com>
2025-01-14 19:14:18 +0700

    add unit testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.dynamic.feature) apply false
 }
